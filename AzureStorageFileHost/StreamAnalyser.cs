8ff3a19790e49fb97f8c0dc6f87a487945630a9e
Tom Eggington <tom@tomeggington.co.uk>
2015-05-29 16:49:29 +0100

    Update max file size to around 40MB. Adjust jpeg recognition

--- AzureStorageFileHost/StreamAnalyser.cs
+++ AzureStorageFileHost/StreamAnalyser.cs
@@ -14,12 +14,12 @@ namespace AzureStorageFileHost
             {
                 case "image/png":
                     return
-                            reader.ReadUInt64() == 0xa1a0a0d474e5089;
+                        reader.ReadUInt64() == 0xa1a0a0d474e5089;
                 case "image/jpeg":
-                    var firstBytes = reader.ReadUInt32();
-                    return
-                            firstBytes == 0xdbffd8ff ||
-                            firstBytes == 0xe1ffd8ff;
+                    //Adapted from http://stackoverflow.com/questions/772388/c-sharp-how-can-i-test-a-file-is-a-jpeg
+                    var startOfImage = reader.ReadUInt16();
+                    var marker = reader.ReadUInt16();
+                    return startOfImage == 0xd8ff && (marker & 0xe0ff) == 0xe0ff;
                 default:
                     return false;
             }
